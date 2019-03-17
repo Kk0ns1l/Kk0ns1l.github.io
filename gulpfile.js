a3e0c7b62af89069391cc76177dd5d46889556cf
Stas Kontsymal <skoncimal@gmail.com>
2019-03-17 16:02:04 +0200

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var browserSync = require('browser-sync');
 
 gulp.task('less', function(){
     return gulp.src('less/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload[{stream: true}])
 });
 
-gulp.task('watch', function(){
-    gulp.watch('less/**/*.less', ['less']) 
+gulp.task('browser-sync', function(){
+    browserSync({
+        server: {
+            baseDir: '/'
+        },
+        notify: false
+    })
 });
+
+gulp.task('watch', ['browser-sync', 'less'], function() {
+    gulp.watch('less/**/*.less', ['less']);
+    gulp.watch('*.html', browserSync.reload);
+ });
