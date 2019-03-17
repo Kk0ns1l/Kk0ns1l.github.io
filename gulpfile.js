29b22fa962cb9326f1a664584b8c2af913d3e4b1
Stas Kontsymal <skoncimal@gmail.com>
2019-03-17 15:37:38 +0200

    watch + less methods gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 gulp.task('less', function(){
-    return gulp.src('less/*.less')
+    return gulp.src('less/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('less/**/*.less', ['less']) 
 });
